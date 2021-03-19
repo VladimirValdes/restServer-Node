5c8fbaedcbae7e3cc31b57638dffc126fc468130
Vladimir Valdes <mendozahum20@gmail.com>
2021-03-19 15:57:02 -0600

    Google SignIn added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,7 +2,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth.controller');
+const { login, googleSignin } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const route = Router();
@@ -11,7 +11,12 @@ route.post('/login',[
     check('correo', 'El correo no es valido').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
-], login)
+], login);
+
+route.post('/google',[
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignin);
 
 
 
